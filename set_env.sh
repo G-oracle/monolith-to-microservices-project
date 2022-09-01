62b6d78aa2e7274ae3244b381ff071b8de3f41fd
G-oracle <akinlolugbemisola@gmail.com>
2022-09-02 00:47:33 +0100

    env

---
+++ set_env.sh
@@ -0,0 +1,12 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+
+ export POSTGRES_USERNAME: gbemisola_mono
+ export POSTGRES_PASSWORD: bramble2022
+ export POSTGRES_HOST: gbemisolards.c4z1xvthbhvc.us-east-1.rds.amazonaws.com
+ export POSTGRES_DB: postgres
+ export AWS_BUCKET: gbemisolas3
+ export AWS_REGION: us-east-1
+ export AWS_PROFILE=default
+ export JWT_SECRET: testing
+ export URL: http://localhost:8100
